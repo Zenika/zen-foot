762bfb692024380820c0dc959302313ea83fb005
Olivier Huber <olivier.huber@zenika.com>
2010-05-27 21:25:20 +0000

    User can be an admin ... or not!

--- src/main/java/com/zenika/zenfoot/model/User.java
+++ src/main/java/com/zenika/zenfoot/model/User.java
@@ -7,6 +7,7 @@ public class User implements Serializable {
     private String password;
     private int points = 0;
     private boolean pending = true;
+    private boolean admin = false;
 
     public User(String email) {
         assert email != null && !email.trim().isEmpty() : "new User: email must not be null";
@@ -34,6 +35,14 @@ public class User implements Serializable {
         int hash = 5;
         hash = 41 * hash + (this.email != null ? this.email.hashCode() : 0);
         return hash;
+    }
+
+    public boolean isAdmin() {
+        return admin;
+    }
+
+    public void setAdmin(boolean admin) {
+        this.admin = admin;
     }
 
     public boolean isPending() {
