8b3f79c994ae96fcaae094c40f5b4d3ee7e109ce
Olivier Huber <olivier.huber@zenika.com>
2010-05-31 23:28:27 +0000

    Cleanup code

--- src/main/java/com/zenika/zenfoot/service/mock/MockMailService.java
+++
@@ -1,9 +0,0 @@
-package com.zenika.zenfoot.service.mock;
-
-import com.zenika.zenfoot.service.MailService;
-
-public class MockMailService implements MailService {
-    public void sendPassword(String email) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-}
