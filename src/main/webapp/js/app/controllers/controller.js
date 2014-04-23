5ff798e678d05c1723744bcb3811761ed8820364
zerbino <r2mart10@gmail.com>
2014-04-23 11:45:49 +0200

    working version of authentication

--- src/main/webapp/js/app/controllers/controller.js
+++ src/main/webapp/js/app/controllers/controller.js
@@ -5,14 +5,34 @@
 'use strict';
 var controllers = angular.module('zenFoot.controllers', ['ngResource']);
 
-controllers.controller('HelloCtrl', [ '$scope','$resource',
+controllers.controller('HelloCtrl', ['$scope','$resource',
 		function($scope, $resource) {
 
 			$scope.sentence= $resource('/api/:param',{param:'coucou'}).get();
 
 		} ]);
 
+/**
+ * This controller sends the login request to restx and spreads an AUTHENTICATED event to scopes.
+ */
+controllers.controller('LoginCtrl', function ($scope, $rootScope, $http, $location) {
+    $scope.login = { };
 
+    $scope.submit = function() {
+        $http.post('/api/sessions',
+            {principal: {name: $scope.login.email, passwordHash: $scope.login.password}},
+            {withCredentials: true}
+        )
+            .success(function(data, status, headers, config) {
+                console.log('authenticated', data, status);
+                $rootScope.$broadcast('AUTHENTICATED', data.principal);
+                $location.path('/index');
+            }).error(function(data, status, headers, config) {
+                console.log('error', data, status);
+                alert("Authentication error, please try again.");
+            });
+    }
+});
 
 
 
