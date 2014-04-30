c4e279b2f4224ab93941038e8899e390dba59b1c
zerbino <r2mart10@gmail.com>
2014-04-30 13:57:26 +0200

    BetService commented

--- src/main/java/com/zenika/zenfoot/gae/services/BetService.java
+++ src/main/java/com/zenika/zenfoot/gae/services/BetService.java
@@ -16,19 +16,9 @@ public class BetService {
         this.betRepository = betRepository;
     }
 
-
-
-
-    public void save(List<Bet> bets, Gambler gambler) {
+public void save(List<Bet> bets, Gambler gambler) {
         //logic on how to add bets (not betting on matchs that are already done/are currently happening.
         List<Bet> formerBets = gambler.getBets();
-
-        for(Bet bet:bets){
-            if(this.hasHappened(bet)){
-
-            }
-        }
-
-        gamblerService.update(gambler);
+        
     }
 }
