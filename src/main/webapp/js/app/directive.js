6b0e0e48f38b2e647eeb518c0570b6f4a8b72ffe
Marjoric Kaneho <marjoric.kaneho@zenika.com>
2014-06-03 16:48:17 +0200

    ZF-9: implementation of form specifications and bug 1 reported in trello fixed. Remains test of confirmation email dispatch

--- src/main/webapp/js/app/directive.js
+++ src/main/webapp/js/app/directive.js
@@ -64,3 +64,18 @@ zenFootDirectives.directive('groupeLabel', function () {
         }
     }
 })
+
+zenFootDirectives.directive('pwdCheck', [function () {
+       	return {
+       		require: 'ngModel',
+       		link: function (scope, elem, attrs, ctrl) {
+       			var firstPassword = '#' + attrs.pwdCheck;
+       			elem.add(firstPassword).on('keyup', function () {
+       				scope.$apply(function () {
+       					var v = elem.val()===$(firstPassword).val();
+       					ctrl.$setValidity('pwdmatch', v);
+       				});
+       			});
+       		}
+       	}
+       }]);
