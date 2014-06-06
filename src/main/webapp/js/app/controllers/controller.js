640f8b144215b9689a2daf7be6275c8b63e4323f
Manuel Boillod <manuel.boillod@zenika.com>
2014-06-06 16:16:44 +0200

    ZF-22 : En tant que parieur, je veux que mon navigateur me propose d'enregistrer mon couple login / password afin de pouvoir me connecter plus rapidement

--- src/main/webapp/js/app/controllers/controller.js
+++ src/main/webapp/js/app/controllers/controller.js
@@ -21,16 +21,22 @@ controllers.controller('LoginCtrl', function ($scope, $rootScope, $http, $locati
     }
 
     $scope.submit = function () {
+        //This method is called by autofill directive, and form validation could not be tested before 
+        //(web browser does not update model values when it auto fill a form)
+        if ($scope.loginForm.$invalid){
+            return;
+        }
         $http.post('/api/sessions',
             {principal: {name: $scope.login.email, passwordHash: $scope.login.password}},
             {withCredentials: true}
         )
             .success(function (data, status, headers, config) {
-                console.log('authenticated', data, status);
+                //console.log('authenticated', data, status);
                 $rootScope.$broadcast('AUTHENTICATED', data.principal);
-                $location.path('/index');
+                //Submit hidden form with classic HTTP POST to enabled password recording in browser
+                angular.element("#postLoginForm").submit();
             }).error(function (data, status, headers, config) {
-                console.log('error', data, status);
+                //console.log('error', data, status);
                 alert("Authentication error, please try again.");
             });
     };
