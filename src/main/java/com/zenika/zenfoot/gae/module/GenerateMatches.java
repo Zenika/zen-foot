65128d2411b70719e41aa42bf0b4a68021f0fc93
mbernardeau <mathias.bernardeau@zenika.com>
2016-06-03 11:16:54 +0200

    Add all Euro 2016 matches

--- src/main/java/com/zenika/zenfoot/gae/module/GenerateMatches.java
+++ src/main/java/com/zenika/zenfoot/gae/module/GenerateMatches.java
@@ -2,6 +2,7 @@ package com.zenika.zenfoot.gae.module;
 
 import com.neovisionaries.i18n.CountryCode;
 import com.zenika.zenfoot.gae.model.Country;
+import com.zenika.zenfoot.gae.services.CountryService;
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
 
@@ -10,6 +11,7 @@ import com.zenika.zenfoot.gae.model.Match;
 import com.zenika.zenfoot.gae.model.Sport;
 import com.zenika.zenfoot.gae.utils.Constants;
 
+import javax.inject.Named;
 import java.util.Locale;
 
 /**
@@ -18,11 +20,55 @@ import java.util.Locale;
 public class GenerateMatches {
 
     //src/main/webapp/
-    public static Match[] generate() {
-                DateTimeZone timezone = Constants.timeZone;
+    public static Match[] generate(CountryService countryService) {
+        DateTimeZone timezone = Constants.timeZone;
+        Country wales = countryService.getByAlpha2Code("gb-wls");
+        Country england = countryService.getByAlpha2Code("gb-eng");
+        Country northernIreland = countryService.getByAlpha2Code("gb-nir");
 
         Match[] matches = {
-                new Match(new DateTime(2014, 6, 12, 22, 0, timezone), "A", new Country(CountryCode.HR), new Country(CountryCode.BR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 10, 21, 0, timezone), "A", new Country(CountryCode.FR), new Country(CountryCode.RO), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 11, 15, 0, timezone), "A", new Country(CountryCode.AL), new Country(CountryCode.CH), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 15, 18, 0, timezone), "A", new Country(CountryCode.RO), new Country(CountryCode.CH), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 15, 21, 0, timezone), "A", new Country(CountryCode.FR), new Country(CountryCode.AL), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 19, 21, 0, timezone), "A", new Country(CountryCode.RO), new Country(CountryCode.AL), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 19, 21, 0, timezone), "A", new Country(CountryCode.CH), new Country(CountryCode.FR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
+                new Match(new DateTime(2016, 6, 11, 18, 0, timezone), "B", wales, new Country(CountryCode.SK), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 11, 21, 0, timezone), "B", england, new Country(CountryCode.RU), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 15, 15, 0, timezone), "B", new Country(CountryCode.RU), new Country(CountryCode.SK), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 16, 15, 0, timezone), "B", england, wales, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 20, 21, 0, timezone), "B", new Country(CountryCode.RU), wales, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 20, 21, 0, timezone), "B", new Country(CountryCode.SK), england, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
+                new Match(new DateTime(2016, 6, 12, 18, 0, timezone), "C", new Country(CountryCode.PL), northernIreland, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 12, 21, 0, timezone), "C", new Country(CountryCode.DE), new Country(CountryCode.UA), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 16, 18, 0, timezone), "C", new Country(CountryCode.UA), northernIreland, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 16, 21, 0, timezone), "C", new Country(CountryCode.DE), new Country(CountryCode.PL), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 21, 18, 0, timezone), "C", new Country(CountryCode.UA), new Country(CountryCode.PL), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 21, 18, 0, timezone), "C", northernIreland, new Country(CountryCode.DE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
+                new Match(new DateTime(2016, 6, 12, 15, 0, timezone), "D", new Country(CountryCode.TR), new Country(CountryCode.HR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 13, 15, 0, timezone), "D", new Country(CountryCode.ES), new Country(CountryCode.CZ), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 17, 18, 0, timezone), "D", new Country(CountryCode.CZ), new Country(CountryCode.HR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 17, 21, 0, timezone), "D", new Country(CountryCode.ES), new Country(CountryCode.TR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 21, 21, 0, timezone), "D", new Country(CountryCode.CZ), new Country(CountryCode.TR), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 21, 21, 0, timezone), "D", new Country(CountryCode.HR), new Country(CountryCode.ES), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
+                new Match(new DateTime(2016, 6, 13, 18, 0, timezone), "E", new Country(CountryCode.IE), new Country(CountryCode.SE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 13, 21, 0, timezone), "E", new Country(CountryCode.BE), new Country(CountryCode.IT), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 17, 15, 0, timezone), "E", new Country(CountryCode.IT), new Country(CountryCode.SE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 18, 15, 0, timezone), "E", new Country(CountryCode.BE), new Country(CountryCode.IE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 22, 21, 0, timezone), "E", new Country(CountryCode.IT), new Country(CountryCode.IE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 22, 21, 0, timezone), "E", new Country(CountryCode.SE), new Country(CountryCode.BE), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
+                new Match(new DateTime(2016, 6, 14, 18, 0, timezone), "F", new Country(CountryCode.AT), new Country(CountryCode.HU), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 14, 21, 0, timezone), "F", new Country(CountryCode.PT), new Country(CountryCode.IS), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 18, 18, 0, timezone), "F", new Country(CountryCode.IS), new Country(CountryCode.HU), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 18, 21, 0, timezone), "F", new Country(CountryCode.PT), new Country(CountryCode.AT), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 22, 18, 0, timezone), "F", new Country(CountryCode.IS), new Country(CountryCode.AT), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+                new Match(new DateTime(2016, 6, 22, 18, 0, timezone), "F", new Country(CountryCode.HU), new Country(CountryCode.PT), new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
+
                /* new Match(new DateTime(2014, 6, 13, 18, 0, timezone), "A", cameroun, mexique, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
                 new Match(new DateTime(2014, 6, 13, 21, 0, timezone), "B", espagne, paysBas, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
                 new Match(new DateTime(2014, 6, 13, 0, 0, timezone), "B", chili, australie, new Sport(SportEnum.FOOTBALL.getIdentifiantSport(),SportEnum.FOOTBALL.getNameSport())),
